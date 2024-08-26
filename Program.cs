9d6c3cca00987c170bc1f6117572356e6a44a0b8
Shakyra Purdy <shakyrapurdy@gmail.com>
2024-08-26 11:42:21 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Fibonacci using Loops:");
+        FibonacciLoop(10);
+
+        Console.WriteLine("\nFibonacci using Recursion:");
+        Console.WriteLine(0);
+        Console.WriteLine(1);
+        FibonacciRecursive(1, 0, 10);
+    }
+
+    static void FibonacciLoop(int n)
+    {
+        int prev2 = 0;
+        int prev1 = 1;
+
+        Console.WriteLine(prev2);
+        Console.WriteLine(prev1);
+
+        for (int i = 2; i < n; i++)
+        {
+            int newFibo = prev1 + prev2;
+            Console.WriteLine(newFibo);
+            prev2 = prev1;
+            prev1 = newFibo;
+        }
+    }
+
+    static void FibonacciRecursive(int prev1, int prev2, int n, int count = 2)
+    {
+        if (count < n)
+        {
+            int newFibo = prev1 + prev2;
+            Console.WriteLine(newFibo);
+            FibonacciRecursive(prev1 + prev2, prev1, n, count + 1);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,19 @@
+The Fibonacci Sequence:
+The Fibonacci numbers is a good way of demonstrating what an algorithm is. 
+The Fibonacci Sequence is a series of numbers where each number is the sum of the two before it.
+
+
+The difference in the loop and recursion model:
+Loop Model:
+A set of instructions that repeats over and over until a certain condition is met.
+The loop keeps executing the same set of instructions (like incrementing the number) until it reaches the end condition.
+Loops are straightforward and run in a fixed pattern.
+
+Recursion Model: 
+A method calls itself. 
+Recursion break into smaller chunks and solve them step by step.
+
+
+The Big O Notation:
+Loop Version: O(n) because it runs a loop n - 2 times
+Recursive Version: O(n) because the method calls itself n - 2 times
